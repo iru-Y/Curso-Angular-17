713e8fc9be7dfcfe7d64f79173673bb9c8aad438
iruy <yuri_matteus@hotmail.com>
2025-08-19 20:04:23 -0300

    create: component para butões

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Card } from "./card/card";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Card],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
